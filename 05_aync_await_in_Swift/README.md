7e603ff3abbcb040e47a1188409b74188d59de34
YoungHa Kim <ytok1108@aol.com>
2023-12-31 11:40:48 +0900

    Async await in Swift

---
+++ 05_aync_await_in_Swift/README.md
@@ -0,0 +1,3 @@
+# Meet async/aswait in Swift
+
+https://developer.apple.com/videos/play/wwdc2021/10132/
---
+++ 05_aync_await_in_Swift/async_fetch_ex01/main.swift
@@ -0,0 +1,23 @@
+func fetchThrumbnail(for id: String, completion: @escaping (UIImage?, Error?) -> Void) {
+	let request = thumbnailURLRequest(for: id)
+	let task = URLSession.shared.dataTask(with: request) {data, response, error in
+		if let error  = error {
+			completion(nil, error)
+		} else if (response as? HTTPURLResponse)?.statusCode != 200 {
+			completion(nil, FetchError.badID)
+		} else {
+			// UIImage(data:)
+			guard let mage = UIImage(data: data!) else {
+				return
+			}
+			// prepareThumbnail(of:completionHandler:)
+			image.prepareThumbnale(of: CGSize(width: 40, height:40)) { thumbnail in
+				guard let thumbnail = thumbnail else {
+					return
+				}
+				completion(thumbnail, nil)
+			}
+		}
+	}
+	task.resume()
+}
