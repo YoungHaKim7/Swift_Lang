bc920d14aa95ad69ec33ed5ddfd8144b4d7f3803
YoungHa Kim <ytok1108@aol.com>
2023-12-31 11:54:15 +0900

    Async await

--- 05_aync_await_in_Swift/async_fetch_ex01/async.swift
+++ 05_aync_await_in_Swift/async_fetch_ex01/async.swift
@@ -8,3 +8,18 @@ func fetchThrumbnail(for id: String) async throws -> UIImage {
 	// thumbnail
 	return thumbnail
 }
+
+extension UIImage {
+	var thumnail: UIImage? {
+		get async {
+			let size = CGSize(width: 40, height: 40)
+			return await self.byPreparingThumbnail(ofSize: size)
+		}
+	}
+}
+
+for await id in staticImageIDsURL.lines {
+	let thumnail = await fetchThumbnail(for: id)
+	collage.add(thumnail)
+}
+let result = await collage.draw() 
