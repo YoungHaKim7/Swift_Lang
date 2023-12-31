809c95712b0b2cc36cb7d5e54d51e47404199de1
YoungHa Kim <ytok1108@aol.com>
2023-12-31 11:45:46 +0900

    Async await in Swift fix Result type

--- 05_aync_await_in_Swift/async_fetch_ex01/main.swift
+++ 05_aync_await_in_Swift/async_fetch_ex01/main.swift
@@ -1,23 +1,23 @@
-func fetchThrumbnail(for id: String, completion: @escaping (UIImage?, Error?) -> Void) {
+func fetchThrumbnail(for id: String, completion: @escaping (Result<UIImage, Error>) -> Void) {
 	let request = thumbnailURLRequest(for: id)
 	let task = URLSession.shared.dataTask(with: request) {data, response, error in
 		if let error  = error {
-			completion(nil, error)
+			completion(.failure(error))
 		} else if (response as? HTTPURLResponse)?.statusCode != 200 {
-			completion(nil, FetchError.badID)
+			completion(.failure(FetchError.badID))
 		} else {
 			// UIImage(data:)
 			guard let mage = UIImage(data: data!) else {
-			completion(nil, FetchError.badImage)
+			completion(.failure(FetchError.badImage))
 				return
 			}
 			// prepareThumbnail(of:completionHandler:)
 			image.prepareThumbnale(of: CGSize(width: 40, height:40)) { thumbnail in
 				guard let thumbnail = thumbnail else {
-					completion(nil, FetchError.badImage)
+					completion(.failure(FetchError.badImage))
 					return
 				}
-				completion(thumbnail, nil)
+				completion(.success(thumbnail))
 			}
 		}
 	}
